136e10eb884a1651d4fbdb312caeaf22d3e16e7e
Kevin Campos <kvnelpdfw@hotmail.com>
2022-07-16 21:32:26 -0500

    Added Email Functionality.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from distance import Iss_Distance
 from my_location import *
+# import smtpdlib
 
+# MY_EMAIL = ?
+# MY_PASSWORD = ?
 iss = Iss_Distance()
 
 # Print to debug code and test current location
@@ -8,3 +11,13 @@ iss = Iss_Distance()
 # print(my_sunrise_sunset())
 
 iss.is_it_close()
+
+# if iss.is_it_close():
+#     connection = smtplib.SMTP("smtp.gmail.com")
+#     connection.starttls()
+#     connection.login(MY_EMAIL, MY_PASSWORD)
+#     Connection.sendmail(
+#         from_addr=MY_EMAIL,
+#         to_addrs=MY_EMAIL,
+#         msg="Subject: Look up! The ISS is above you."
+    )
